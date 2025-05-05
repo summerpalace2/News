ac6c060a999512f70a44d64fa40d01e287e55931
Your Name <you@example.com>
2025-05-05 19:53:26 +0800

    Banner

--- app/src/main/java/com/example/testwxy/ui/NewsActivity.kt
+++ app/src/main/java/com/example/testwxy/ui/NewsActivity.kt
@@ -19,18 +19,19 @@ import com.example.testwxy.viewmodel.MainViewModel
 
 
 class NewsActivity : AppCompatActivity() {
-    private var id: Int?=null
-    private var url: String?=null
-    private var number=0
+    private var id: Int? = null
+    private var url: String? = null
+    private var number = 0
     private lateinit var commentAdapter: NewsUrladapter
-    private lateinit var viewpager:ViewPager2
+    private lateinit var viewpager: ViewPager2
     private var previousPosition = 0
-    private val viewModel: MainViewModel by lazy{
+    private val viewModel: MainViewModel by lazy {
         MainViewModel(application)
     }
     private val binding: ActivityNewsBinding by lazy {
         ActivityNewsBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -48,9 +49,9 @@ class NewsActivity : AppCompatActivity() {
         binding.tvReturn.setOnClickListener {
             finish()
         }
-        binding.thinkimage.setOnClickListener{
+        binding.thinkimage.setOnClickListener {
             intent = Intent(this, CommentActivity::class.java)
-            intent.putExtra("id",id)
+            intent.putExtra("id", id)
             startActivity(intent)
 
         }
@@ -79,8 +80,8 @@ class NewsActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        val date=intent.getStringExtra("date")?:" "
-        viewpager=binding.webViewviewpager
+        val date = intent.getStringExtra("date") ?: " "
+        viewpager = binding.webViewviewpager
         listener(date)
         getOnceListUrl(date)
 
@@ -106,105 +107,102 @@ class NewsActivity : AppCompatActivity() {
                 val currentStory = commentAdapter.getItem(position)  // 获取当前新闻
                 currentStory?.let {
                     getcomment(it.id) // 获取评论
-                    id=it.id//把id传递到下一个activity
-                    url=it.url
+                    id = it.id//把id传递到下一个activity
+                    url = it.url
                 }
 
             }
         })
     }
 
-    private fun getcomment(id:Int){
-       viewModel.getnumberComment(id)
+    private fun getcomment(id: Int) {
+        viewModel.getnumberComment(id)
     }
 
-    private fun getOnceListUrl(date:String){
-        Log.d("dateNews",date)
-        if(date== Tool.getDateNews()){
+    private fun getOnceListUrl(date: String) {
+        Log.d("dateNews", date)
+        if (date == Tool.getDateNews()) {
             viewModel.getNews()
-        }
-        else{
+        } else {
             viewModel.getRecentNews(Tool.getNextDay(date))
         }
 
     }
-    private fun getTwiceListUrl(date:String){
-        Log.d("dateNews",date)
+
+    private fun getTwiceListUrl(date: String) {
+        Log.d("dateNews", date)
         viewModel.getRecentNewsUrl(Tool.getNextDay(date))
     }
 
-    private fun observe(){
-        viewModel.newsResult.observe(this){
-            result ->
-                when (result) {
-                    is ResultData.Success -> {
-                        Log.d("NewsActivity", "news: $result")
-                        val news = result.data
-                        commentAdapter.updataData(news.stories)
-                        for(i in 0 until news.stories.size){
-                            if(news.stories[i].url == (intent.getStringExtra("url") ?: "")){
-                                viewpager.setCurrentItem(i, true)
-                            }
+    private fun observe() {
+        viewModel.newsResult.observe(this) { result ->
+            when (result) {
+                is ResultData.Success -> {
+                    Log.d("NewsActivity", "news: $result")
+                    val news = result.data
+                    commentAdapter.updataData(news.stories)
+                    for (i in 0 until news.stories.size) {
+                        if (news.stories[i].url == (intent.getStringExtra("url") ?: "")) {
+                            viewpager.setCurrentItem(i, true)
                         }
                     }
-
-                    is ResultData.Error -> {
-                        Toast.makeText(this, "网络错误", Toast.LENGTH_SHORT).show()
-
-                    }
                 }
 
-        }
-        viewModel.recentNewsResult.observe(this){
-            result ->
-                when (result) {
-                    is ResultRecentData.Success -> {
-                        Log.d("NewsActivity", "news: $result")
-                        val news = result.data
-                        commentAdapter.updataData(news.stories)
-                        viewpager.setCurrentItem(1, true)
-                    }
+                is ResultData.Error -> {
+                    Toast.makeText(this, "网络错误", Toast.LENGTH_SHORT).show()
 
-                    is ResultRecentData.Error -> {
-                        Toast.makeText(this, "网络错误", Toast.LENGTH_SHORT).show()
-
-                   }
                 }
-        }
+            }
 
-        viewModel.resulturlRecentNewsResult.observe(this){
-            result ->
-                when (result) {
-                    is ResultUrlRecentData.Success -> {
-                        Log.d("RecentData", "news: $result")
-                        val news = result.data
-                        commentAdapter.updataData(news.stories)
-                    }
-                    is ResultUrlRecentData.Error -> {
-                        Toast.makeText(this, "网络错误", Toast.LENGTH_SHORT).show()
-                    }
+        }
+        viewModel.recentNewsResult.observe(this) { result ->
+            when (result) {
+                is ResultRecentData.Success -> {
+                    Log.d("NewsActivity", "news: $result")
+                    val news = result.data
+                    commentAdapter.updataData(news.stories)
+                    viewpager.setCurrentItem(1, true)
                 }
-        }
-        viewModel.commentnumberResult.observe(this){
-            result ->
-                when (result) {
-                    is CommentnumberResult.Success -> {
-                        Log.d("commentData", "news: $result")
-                        val comment = result.data
-                        binding.thinknumber.text = "评论数：${comment.comments}"
-                        binding.likenumber.text = "喜欢数：${comment.popularity}"
 
-                        Log.d("comment",comment.toString())
-                    }
-                    is CommentnumberResult.Error -> {
-                        Toast.makeText(this, "返回错误", Toast.LENGTH_SHORT).show()
-                    }
+                is ResultRecentData.Error -> {
+                    Toast.makeText(this, "网络错误", Toast.LENGTH_SHORT).show()
+
                 }
+            }
         }
 
+        viewModel.resulturlRecentNewsResult.observe(this) { result ->
+            when (result) {
+                is ResultUrlRecentData.Success -> {
+                    Log.d("RecentData", "news: $result")
+                    val news = result.data
+                    commentAdapter.updataData(news.stories)
+                }
+
+                is ResultUrlRecentData.Error -> {
+                    Toast.makeText(this, "网络错误", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        viewModel.commentnumberResult.observe(this) { result ->
+            when (result) {
+                is CommentnumberResult.Success -> {
+                    Log.d("commentData", "news: $result")
+                    val comment = result.data
+                    binding.thinknumber.text = "评论数：${comment.comments}"
+                    binding.likenumber.text = "喜欢数：${comment.popularity}"
+
+                    Log.d("comment", comment.toString())
+                }
+
+                is CommentnumberResult.Error -> {
+                    Toast.makeText(this, "返回错误", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
 
     }
-
-
 }
+
+
